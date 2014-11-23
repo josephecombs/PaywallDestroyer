042593a19e609a3b23386166dc3e4415e6797d7e
Joseph Combs <joseph.e.combs@gmail.com>
2014-11-23 00:16:57 -0800

    figuring some things out

--- test.rb
+++ test.rb
@@ -17,7 +17,8 @@ def get_credentials
     consumer_key_string: temp_arr[0],
     consumer_secret_string: temp_arr[1],
     access_token_string: temp_arr[2],
-    access_secret_string: temp_arr[3]
+    access_secret_string: temp_arr[3],
+    google_api_key: temp_arr[4]
   }
   
   keys
@@ -28,16 +29,33 @@ def get_new_tweets(target_user, consumer_key, access_token)
   
 end
 
-def get_google_link(headline)
+def get_top_google_link(headline, google_api_key)
+  url = google_headlineify(headline, google_api_key)
+  # doc = JSON.parse(open(url))
+  # puts doc
+  # sleep 100
+  
+  address = URI(url)
+  request = Net::HTTP::Get.new address.request_uri
+  http = Net::HTTP.new address.host, address.port
+  
+  http.start
+  response = http.request(request)
+  results = JSON.parse(response.body)
+  
+  puts results
+  # top_search_result = doc.css('li.card-section')
+  # puts top_search_result
 #https://www.google.com/#q=Russia+A+wounded+economy+It+is+closer+to+crisis+than+the+West+or+Vladimir+Putin+realized
 end
 
-def google_headlineify(text)
+def google_headlineify(text, google_api_key)
   words_arr = text.split(" ")
-  url = "http://www.google.com/#q="
+  url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=AIzaSyCRLjxxwTkiHqYBVSp8LBrs31q9mcxzAGc&q="
   words_arr.each do |word|
     url += "+#{word}"
-  end  
+  end
+  
   url
 end
 
@@ -67,6 +85,6 @@ KEYS = get_credentials
 consumer_key = OAuth::Consumer.new(KEYS[:consumer_key_string], KEYS[:consumer_secret_string])
 access_token = OAuth::Token.new(KEYS[:access_token_string], KEYS[:access_secret_string])
 
-puts google_headlineify("Just a test news article headline")
+get_top_google_link("Russia A Wounded economy It is closer to crisis than the West or Vladimir Putin realized", KEYS[:google_api_key])
 
 #send_response_tweet("536245096811745280", "@josephcombs", "test-google-linKK.com", consumer_key, access_token)
