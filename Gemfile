bd827ecc399926bde9c9ae6d91940d6032e3164f
Joseph Combs <joseph.e.combs@gmail.com>
2014-11-22 22:40:58 -0800

    adding more functionality

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 gem 'oauth'
 gem 'json'
+gem 'nokogiri'
--- test.rb
+++ test.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'oauth'
 require 'json'
 require 'open-uri'
+require 'nokogiri'
 
 def get_credentials
   temp_arr = []
@@ -9,7 +10,6 @@ def get_credentials
   #get your own keys if you are going to use this --> apps.twitter.com
   File.readlines('keys.txt').each do |line, idx|
     temp_arr[i] = line.gsub!("\n","")
-    puts "aaa"
     i += 1
   end
   
@@ -28,17 +28,26 @@ def get_new_tweets(target_user, consumer_key, access_token)
   
 end
 
-def get_google_link
-  
+def get_google_link(headline)
+#https://www.google.com/#q=Russia+A+wounded+economy+It+is+closer+to+crisis+than+the+West+or+Vladimir+Putin+realized
 end
 
-def send_response_tweet(tweet_id, google_link, consumer_key, access_token)
+def google_headlineify(text)
+  words_arr = text.split(" ")
+  url = "http://www.google.com/#q="
+  words_arr.each do |word|
+    url += "+#{word}"
+  end  
+  url
+end
+
+def send_response_tweet(tweet_id, user, google_link, consumer_key, access_token)
   baseurl = "https://api.twitter.com"
   path    = "/1.1/statuses/update.json"
   address = URI("#{baseurl}#{path}")
   request = Net::HTTP::Post.new address.request_uri
   request.set_form_data(
-    status: "helpful link: #{google_link}",
+    status: "helpful link for #{user} readers: #{google_link}",
     in_reply_to_status_id: tweet_id
   )
 
@@ -58,4 +67,6 @@ KEYS = get_credentials
 consumer_key = OAuth::Consumer.new(KEYS[:consumer_key_string], KEYS[:consumer_secret_string])
 access_token = OAuth::Token.new(KEYS[:access_token_string], KEYS[:access_secret_string])
 
-send_response_tweet(536245096811745280, "test-google-link.com", consumer_key, access_token)
+puts google_headlineify("Just a test news article headline")
+
+#send_response_tweet("536245096811745280", "@josephcombs", "test-google-linKK.com", consumer_key, access_token)
