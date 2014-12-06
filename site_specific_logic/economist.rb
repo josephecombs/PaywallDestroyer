34fc5cebb8c7fba22a2d6bd2221a0a2257f2ddb3
Joseph Combs <joseph.e.combs@gmail.com>
2014-12-05 20:29:49 -0800

    fixing some economist selectors

--- site_specific_logic/economist.rb
+++ site_specific_logic/economist.rb
@@ -1,7 +1,7 @@
 def economist_fetch_headline(url)
   #this method visits an economist.com url and returns the headline and subheadline of the story 
   doc = Nokogiri::HTML(open(url))
-  h2 = doc.css('h2.fly-title').text
+  h2 = doc.css('fly-title').text
   h3 = doc.css('h3.headline').text
   raw_headline = h2 + " " + h3
   raw_headline
