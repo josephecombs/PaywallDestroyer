6e79127de7a8f17eb427b859dbf3b187ea0451ed
Joseph Combs <joseph.e.combs@gmail.com>
2014-12-07 17:58:36 -0800

    more testing

--- site_specific_logic/new_york_times.rb
+++ site_specific_logic/new_york_times.rb
@@ -2,7 +2,6 @@ def new_york_times_fetch_headline(url)
   doc = Nokogiri::HTML(open(url))
   # doc = Nokogiri::HTML(open('http://www.economist.com/news/leaders/21633813-it-closer-crisis-west-or-vladimir-putin-realise-wounded-economy'))
   h2 = doc.css('.story-heading').text
-  h1 = doc.css('h1.deck').text
-  raw_headline = h2 + " " + h1 + " NYT"
+  raw_headline = h2 + " NYT"
   raw_headline
 end
