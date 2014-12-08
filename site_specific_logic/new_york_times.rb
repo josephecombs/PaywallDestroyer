165d13ca6363ab01e5ff1c1a72bcac8a5d1aade7
Joseph Combs <joseph.e.combs@gmail.com>
2014-12-07 17:24:37 -0800

    minor myt fix I think

--- site_specific_logic/new_york_times.rb
+++ site_specific_logic/new_york_times.rb
@@ -1,7 +1,7 @@
 def new_york_times_fetch_headline(url)
   doc = Nokogiri::HTML(open(url))
   # doc = Nokogiri::HTML(open('http://www.economist.com/news/leaders/21633813-it-closer-crisis-west-or-vladimir-putin-realise-wounded-economy'))
-  h2 = doc.css('h2.story-heading').text
+  h2 = doc.css('.story-heading').text
   h1 = doc.css('h1.deck').text
   raw_headline = h2 + " " + h1 + " NYT"
   raw_headline
